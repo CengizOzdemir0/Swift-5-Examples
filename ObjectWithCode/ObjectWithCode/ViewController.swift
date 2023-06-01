b51ad09aaf3b542cd7ae8d52f972a73644176f4a
CengizOzdemir0 <63748401+CengizOzdemir0@users.noreply.github.com>
2023-06-01 11:35:00 +0300

    ObjectwithCode

---
+++ ObjectWithCode/ObjectWithCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectWithCode
+//
+//  Created by sys on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    let myLabel = UILabel()
+   dsasdasdasd
+
+}
+
