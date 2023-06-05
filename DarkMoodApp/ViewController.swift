111bacbd8af1a460e801c0d8345dd23ac3ee4d1a
cozdemir <cengiz.ozdemir@assistt.com.tr>
2023-06-05 09:51:32 +0300

    traitCollectionDidChange

--- DarkMoodApp/ViewController.swift
+++ DarkMoodApp/ViewController.swift
@@ -8,23 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
+        
     }
     override func viewWillAppear(_ animated: Bool) {
         let userTraitCollection = traitCollection.userInterfaceStyle
-         if userTraitCollection == .dark {
-             changeButton.tintColor = UIColor.white
-         }else{
-             changeButton.tintColor = UIColor.blue
-
-         }
+        if userTraitCollection == .dark {
+            changeButton.tintColor = UIColor.white
+        }else{
+            changeButton.tintColor = UIColor.blue
+            
+        }
     }
-   
-
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        let userTraitCollection = traitCollection.userInterfaceStyle
+        if userTraitCollection == .dark {
+            changeButton.tintColor = UIColor.white
+        }else{
+            changeButton.tintColor = UIColor.blue
+            
+        }
+    }
+    
 }
 
