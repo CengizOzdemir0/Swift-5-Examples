39190871745b750df2f821fec4de03e4c8a1bb70
CengizOzdemir0 <63748401+CengizOzdemir0@users.noreply.github.com>
2023-06-05 09:59:27 +0300

    Add files via upload

---
+++ FaceRecognitionProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceRecognitionProject
+//
+//  Created by sys on 5.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
